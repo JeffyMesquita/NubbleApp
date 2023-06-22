4c4cd1f561a46e897803e1fb9001af02d064365f
Jeferson Mesquita <je_2742@hotmail.com>
2023-06-22 19:07:18 -0300

    TextInput - Introdução

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
-import { ThemeProvider } from '@shopify/restyle';
+import {ThemeProvider} from '@shopify/restyle';
 import React from 'react';
-import { SafeAreaView, View } from 'react-native';
+import {SafeAreaView, TextInput, View} from 'react-native';
 
-import { Text } from './src/components/Text/Text';
+import {Text} from './src/components/Text/Text';
 
-import { Icon } from './src/components/Icons/Icon';
-import { theme } from './src/theme/theme';
+import {theme} from './src/theme/theme';
+import { Box } from './src/components/Box/Box';
+import { Button } from './src/components/Button/Button';
 
 function App(): JSX.Element {
   return (
@@ -14,47 +15,47 @@ function App(): JSX.Element {
         <View
           style={{
             paddingHorizontal: 24,
-            gap: 16,
           }}>
-          <Text preset="headingLarge">Coffstack</Text>
+          <Text mb="s8" preset="headingLarge">
+            Olá!
+          </Text>
+          <Text mb="s40" preset="paragraphLarge">
+            Digite seu e-mail e senha para entrar
+          </Text>
 
-          <View
-            style={{
-              flexDirection: 'row',
-            }}>
-            <View>
-              <Icon name="arrowLeft" color="error" size={35} />
-              <Icon name="arrowRight" color="error" size={35} />
-              <Icon name="bell" color="error" size={35} />
-              <Icon name="bellOn" color="error" size={35} />
-              <Icon name="bookmark" color="error" size={35} />
-              <Icon name="bookmarkFill" color="error" size={35} />
-              <Icon name="camera" color="error" size={35} />
-              <Icon name="chat" color="error" size={35} />
-              <Icon name="chatOn" color="error" size={35} />
-              <Icon name="check" color="error" size={35} />
-              <Icon name="chevronRight" color="error" size={35} />
-              <Icon name="comment" color="error" size={35} />
-              <Icon name="eyeOff" color="carrotSecondary" size={35} />
-              <Icon name="eyeOn" color="error" size={35} />
-              <Icon name="flashOff" color="error" size={35} />
-              <Icon name="flashOn" color="error" size={35} />
-            </View>
-            <View>
-              <Icon name="heart" color="error" size={35} />
-              <Icon name="heartFill" color="error" size={35} />
-              <Icon name="home" color="error" size={35} />
-              <Icon name="homeFill" color="error" size={35} />
-              <Icon name="homeFill" color="error" size={35} />
-              <Icon name="message" color="error" size={35} />
-              <Icon name="newPost" color="error" size={35} />
-              <Icon name="profile" color="error" size={35} />
-              <Icon name="profileFill" color="error" size={35} />
-              <Icon name="search" color="error" size={35} />
-              <Icon name="settings" color="error" size={35} />
-              <Icon name="trash" color="error" size={35} />
-            </View>
-          </View>
+          <Box mb="s20">
+            <Text mb="s4" preset="paragraphLarge">
+              E-mail
+            </Text>
+            <TextInput
+              placeholder="Digite seu e-mail"
+              style={{
+                borderWidth: 1,
+                height: 50,
+              }}
+            />
+          </Box>
+
+          <Box>
+            <Text mb="s4" preset="paragraphLarge">
+              Senha
+            </Text>
+            <TextInput
+              placeholder="Digite sua senha"
+              style={{
+                borderWidth: 1,
+                height: 50,
+              }}
+            />
+          </Box>
+
+          <Text mt="s10" color="primary" preset="paragraphSmall" bold>
+            Esqueci minha senha
+          </Text>
+
+          <Button title="Entrar" mt="s48" />
+
+          <Button title="Criar uma conta" mt="s12" preset='outline' />
         </View>
       </SafeAreaView>
     </ThemeProvider>
