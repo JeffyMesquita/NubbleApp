dc383afe7c2c9331b1027131d5f73581236d457f
Jeferson Mesquita <je_2742@hotmail.com>
2023-06-23 08:24:17 -0300

    Melhorando a UX com onPress

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -1,46 +1,56 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import {
+  Pressable,
   TextInput as RNTextInput,
   TextInputProps as RNTextInputProps,
   TextStyle,
 } from 'react-native';
 import {Box, BoxProps} from '../Box/Box';
 import {$fontFamily, $fontSizes, Text} from '../Text/Text';
-import { useAppTheme } from '../../hooks/useAppTheme';
+import {useAppTheme} from '../../hooks/useAppTheme';
 
 interface TextInputProps extends RNTextInputProps {
   label: string;
 }
 
 export function TextInput({label, ...rnTextInputProps}: TextInputProps) {
-  const { colors } = useAppTheme();
+  const {colors} = useAppTheme();
+  const inputRef = useRef<RNTextInput>(null);
+
+  function focusInput() {
+    inputRef.current?.focus();
+  }
 
   return (
-    <Box>
-      <Text mb="s4" preset="paragraphMedium" bold>
-        {label}
-      </Text>
-      <Box {...$textInputContainer}>
-        <RNTextInput
-          placeholderTextColor={colors.gray4}
-          style={$textInputStyle}
-          {...rnTextInputProps}
-        />
+    <Pressable
+      onPress={focusInput}
+    >
+      <Box>
+        <Text mb="s4" preset="paragraphMedium" bold>
+          {label}
+        </Text>
+        <Box {...$textInputContainer}>
+          <RNTextInput
+            ref={inputRef}
+            placeholderTextColor={colors.gray4}
+            style={$textInputStyle}
+            {...rnTextInputProps}
+          />
+        </Box>
       </Box>
-    </Box>
+    </Pressable>
   );
 }
 
 const $textInputStyle: TextStyle = {
-  borderWidth: 1,
   padding: 0,
   fontFamily: $fontFamily.regular,
   ...$fontSizes.paragraphMedium,
-}
+};
 
 const $textInputContainer: BoxProps = {
   borderWidth: 1,
   borderColor: 'gray4',
   borderRadius: 's12',
   padding: 's16',
-}
+};
