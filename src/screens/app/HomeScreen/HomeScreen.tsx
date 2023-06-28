e631814ae9aa3db196c6202266f4af75dfb7ed56
Jeferson Mesquita <je_2742@hotmail.com>
2023-06-28 08:45:17 -0300

    Feat:: Navegação e Typescript com Nested Navigator

--- src/screens/app/HomeScreen/HomeScreen.tsx
+++ src/screens/app/HomeScreen/HomeScreen.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 
 import {Button, Screen, Text} from '@components';
-import {AppScreenProps} from '@routes';
+import {AppTabScreenProps} from '@routes';
 
-export function HomeScreen({navigation}: AppScreenProps<'HomeScreen'>) {
+export function HomeScreen({navigation}: AppTabScreenProps<'HomeScreen'>) {
   return (
     <Screen>
       <Text preset="headingLarge">Home Screen</Text>
       <Button
         title="Settings"
         onPress={() => navigation.navigate('SettingsScreen')}
+        mb="s20"
+      />
+
+      <Button
+        title="Favorites"
+        onPress={() => navigation.navigate('FavoriteScreen')}
       />
     </Screen>
   );
