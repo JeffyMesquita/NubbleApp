dcbac6c53031413c6528d43ca4fcf176fd99bfd9
Jeferson Mesquita <je_2742@hotmail.com>
2023-06-28 08:54:33 -0300

    Customização Bottom Tab - Introdução

--- src/routes/AppTabNavigator.tsx
+++ src/routes/AppTabNavigator.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import {Icon, Text} from '@components';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {
   HomeScreen,
@@ -23,7 +24,26 @@ export function AppTabNavigator() {
         headerShown: false,
       }}
       initialRouteName="HomeScreen">
-      <Tab.Screen name="HomeScreen" component={HomeScreen} />
+      <Tab.Screen
+        name="HomeScreen"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: ({focused}) => (
+            <Text
+              preset="paragraphCaption"
+              semiBold
+              color={focused ? 'primary' : 'backgroundContrast'}>
+              Início
+            </Text>
+          ),
+          tabBarIcon: ({focused}) => (
+            <Icon
+              name={focused ? 'homeFill' : 'home'}
+              color={focused ? 'primary' : 'backgroundContrast'}
+            />
+          ),
+        }}
+      />
       <Tab.Screen name="NewPostScreen" component={NewPostScreen} />
       <Tab.Screen name="FavoriteScreen" component={FavoriteScreen} />
       <Tab.Screen name="MyProfileScreen" component={MyProfileScreen} />
