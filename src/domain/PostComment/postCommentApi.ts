f6c096dd03927bb9ea74c8ae9e410921bebdfbaa
Jeferson Mesquita <je_2742@hotmail.com>
2023-07-28 12:33:13 -0300

    :zap: feat:: Domínio, API e Service

---
+++ src/domain/PostComment/postCommentApi.ts
@@ -0,0 +1,21 @@
+import {PageAPI, PageParams, api} from '@api';
+
+import {PostCommentAPI} from './postCommentTypes';
+
+async function getList(
+  post_id: number,
+  pageParams: PageParams,
+): Promise<PageAPI<PostCommentAPI>> {
+  const response = await api.get<PageAPI<PostCommentAPI>>('user/post_comment', {
+    params: {
+      ...pageParams,
+      post_id,
+    },
+  });
+
+  return response.data;
+}
+
+export const postCommentApi = {
+  getList,
+};
---
+++ src/domain/PostComment/postCommentTypes.ts
@@ -0,0 +1,31 @@
+export interface PostComment {
+  id: number; // 107;
+  message: string; // 'Ad excepturi reiciendis.';
+  createdAt: string; // '2023-07-27T01:47:42.000-03:00';
+  author: {
+    id: number; // 8;
+    profileUrl: string; // 'https://nubble-development.s3.sa-east-1.amazonaws.com/backend-integration/3-gabriel.png';
+    name: string; // 'Gabriel Lemos';
+    userName: string; // 'glemos';
+  };
+}
+
+export interface PostCommentAPI {
+  id: number; // 107;
+  message: string; // 'Ad excepturi reiciendis.';
+  user_id: number; // 8;
+  post_id: number; // 1;
+  created_at: string; // '2023-07-27T01:47:42.000-03:00';
+  updated_at: string; // '2023-07-27T16:12:25.820-03:00';
+  user: {
+    id: number; // 8;
+    first_name: string; // 'Gabriel';
+    last_name: string; // 'Lemos';
+    username: string; // 'glemos';
+    email: string; // 'glemos@coffstack.com';
+    profile_url: string; // 'https://nubble-development.s3.sa-east-1.amazonaws.com/backend-integration/3-gabriel.png';
+    is_online: boolean; // false;
+    full_name: string; // 'Gabriel Lemos';
+  };
+  meta: any; // {};
+}
