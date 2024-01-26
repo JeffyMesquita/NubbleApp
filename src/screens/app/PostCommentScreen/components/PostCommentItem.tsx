e566d94932d0db5a44bc86f1553ac0f1457f2ef5
Jeferson Antonio Mesquita <je_2742@hotmail.com>
2024-01-26 15:28:47 -0300

    feat Remover Comentário - Integração da UI com usePostCommentRemove

--- src/screens/app/PostCommentScreen/components/PostCommentItem.tsx
+++ src/screens/app/PostCommentScreen/components/PostCommentItem.tsx
@@ -1,24 +1,42 @@
 import React from 'react';
+import {Alert, Pressable} from 'react-native';
 
 import {Box, ProfileAvatar, Text} from '@components';
-import {PostComment} from '@domain';
+import {PostComment, usePostCommentRemove} from '@domain';
 
 interface Props {
   postComment: PostComment;
 }
 
 export function PostCommentItem({postComment}: Props) {
+  const {mutate} = usePostCommentRemove();
+
+  function confirmRemove() {
+    Alert.alert('Deseja excluir o comentário?', 'pressione confirmar', [
+      {
+        text: 'Confirmar',
+        onPress: () => mutate({postCommentId: postComment.id}),
+      },
+      {
+        text: 'Cancelar',
+        style: 'cancel',
+      },
+    ]);
+  }
+
   return (
-    <Box mb="s16" flexDirection="row" gap="s12" alignItems="center">
-      <ProfileAvatar imageURL={postComment.author.profileUrl} />
-      <Box flex={1}>
-        <Text preset="paragraphSmall" bold>
-          {postComment.author.userName}
-        </Text>
-        <Text preset="paragraphSmall" color="gray1">
-          {postComment.message} - {postComment.createdAtRelative}
-        </Text>
+    <Pressable onLongPress={confirmRemove}>
+      <Box mb="s16" flexDirection="row" gap="s12" alignItems="center">
+        <ProfileAvatar imageURL={postComment.author.profileUrl} />
+        <Box flex={1}>
+          <Text preset="paragraphSmall" bold>
+            {postComment.author.userName}
+          </Text>
+          <Text preset="paragraphSmall" color="gray1">
+            {postComment.message} - {postComment.createdAtRelative}
+          </Text>
+        </Box>
       </Box>
-    </Box>
+    </Pressable>
   );
 }
