fa75dafecb51b7570945c2cd87a18807032f9f69
Jeferson Antonio Mesquita <je_2742@hotmail.com>
2024-02-20 09:22:56 -0300

    feat: DRY - Hook Generics para username e email

--- src/domain/Auth/useCases/useAuthIsValueAvailable.ts
+++ src/domain/Auth/useCases/useAuthIsValueAvailable.ts
@@ -4,27 +4,64 @@ import {useQuery} from '@tanstack/react-query';
 
 import {authService} from '../authService';
 
-interface Param {
-  username: string;
+interface Param<T extends {length: number}> {
+  value: T;
   enable: boolean;
+  queryKey: QueryKeys;
+  isAvailableFunc: (value: T) => Promise<boolean>;
 }
 
-export function useAuthIsUsernameAvailable({username, enable}: Param) {
-  const debouncedUsername = useDebounce(username, 1500);
+function useAuthIsValueAvailable<T extends {length: number}>({
+  value,
+  enable,
+  queryKey,
+  isAvailableFunc,
+}: Param<T>) {
+  const debouncedValue = useDebounce(value, 1500);
 
   const {data, isFetching} = useQuery({
-    queryKey: [QueryKeys.IsUserNameAvailable, debouncedUsername],
-    queryFn: () => authService.isUserNameAvailable(debouncedUsername),
+    queryKey: [queryKey, debouncedValue],
+    queryFn: () => isAvailableFunc(debouncedValue),
     retry: false,
     staleTime: 20000,
-    enabled: enable && debouncedUsername.length > 0,
+    enabled: enable && debouncedValue.length > 0,
   });
 
-  const isDebouncing = debouncedUsername !== username;
+  const isDebouncing = debouncedValue !== value;
 
   return {
     isAvailable: !!data,
     isUnavailable: data === false,
     isFetching: isFetching || isDebouncing,
   };
+}
+
+export function useAuthIsUsernameAvailable({
+  username,
+  enable,
+}: {
+  username: string;
+  enable: boolean;
+}) {
+  return useAuthIsValueAvailable({
+    value: username,
+    enable,
+    queryKey: QueryKeys.IsUsernameAvailable,
+    isAvailableFunc: authService.isUserNameAvailable,
+  });
+}
+
+export function useAuthIsEmailAvailable({
+  email,
+  enable,
+}: {
+  email: string;
+  enable: boolean;
+}) {
+  return useAuthIsValueAvailable({
+    value: email,
+    enable,
+    queryKey: QueryKeys.IsEmailAvailable,
+    isAvailableFunc: authService.isEmailAvailable,
+  });
 }
