dfb64c9a6a6c7588b729d7b1cfeeacb3ec02b7cf
Jeferson Antonio Mesquita <je_2742@hotmail.com>
2024-10-09 22:38:18 -0300

    feat: describe: Agrupe e Organize seu Teste

--- src/utils/__tests__/stringUtils.test.ts
+++ src/utils/__tests__/stringUtils.test.ts
@@ -1,12 +1,28 @@
 import {stringUtils} from '@utils';
 
-test('capitalizeFirstLetter', () => {
-  // escrever meu teste
-  stringUtils.capitalizeFirstLetter('Ana maria');
-  stringUtils.capitalizeFirstLetter('ANA MARIA');
-  stringUtils.capitalizeFirstLetter('MaRIA');
+describe('stringUtils', () => {
+  describe('capitalizeFirstLetter', () => {
+    it('should capitalize the first letter of each word', () => {
+      // escrever meu teste
+      stringUtils.capitalizeFirstLetter('Ana maria');
+      stringUtils.capitalizeFirstLetter('ANA MARIA');
+      stringUtils.capitalizeFirstLetter('MaRIA');
 
-  const nome = stringUtils.capitalizeFirstLetter('Ana maria');
+      expect(stringUtils.capitalizeFirstLetter('Ana maria')).toBe('Ana Maria');
+      expect(stringUtils.capitalizeFirstLetter('ANA MARIA')).toBe('Ana Maria');
+      expect(stringUtils.capitalizeFirstLetter('MaRIA')).toBe('Maria');
+      expect(stringUtils.capitalizeFirstLetter('ana maria')).toBe('Ana Maria');
+      expect(stringUtils.capitalizeFirstLetter('ana')).toBe('Ana');
+      expect(stringUtils.capitalizeFirstLetter('MARIA')).toBe('Maria');
+    });
 
-  expect(nome).toBe('Ana Maria');
+    it('should return an empty string when the input is empty', () => {
+      expect(stringUtils.capitalizeFirstLetter('')).toBe('');
+    });
+
+    it('should remove leading/trailing spaces', () => {
+      expect(stringUtils.capitalizeFirstLetter(' Ana maria')).toBe('Ana Maria');
+      expect(stringUtils.capitalizeFirstLetter('Ana maria ')).toBe('Ana Maria');
+    });
+  });
 });
