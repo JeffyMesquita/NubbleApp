d55155534485e2857f42296f3c44c74286da8614
Jeferson Antonio Mesquita <je_2742@hotmail.com>
2024-10-29 22:23:11 -0300

    feat: Testando propriedades de estilo (style)

--- src/components/Button/__tests__/Button.test.tsx
+++ src/components/Button/__tests__/Button.test.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import {StyleSheet} from 'react-native';
 
+import {theme} from '@theme';
 import {render, fireEvent, screen} from 'test-utils';
 
 import {Button, ButtonProps} from '../Button';
@@ -36,5 +38,13 @@ describe('<Button />', () => {
     fireEvent.press(titleElement);
 
     expect(mockedOnPress).not.toHaveBeenCalled();
+  });
+
+  test('the title should be gray if the button is disabled', () => {
+    const {titleElement} = renderComponent({disabled: true});
+
+    const titleStyle = StyleSheet.flatten(titleElement.props.style);
+
+    expect(titleStyle.color).toEqual(theme.colors.gray2);
   });
 });
